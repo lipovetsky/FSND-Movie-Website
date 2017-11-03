da1b0c10913b0435b3610f061c769c7f7b866a87
User <user@Users-iMac.local>
2017-11-03 11:49:17 -0400

    Pep8 the Files

--- entertainment.py
+++ entertainment.py
@@ -1,14 +1,22 @@
 import fresh_tomatoes
 import movie
 
-# Here, we import the two other .py files/modules: movie and fresh_tomatoes. Movie is where we have created the appropriate classes.
-# We can create a new instance of a class by defining a variable as movie.Movie. Movie with the upper-case is the class, and movie with
-# the lower case is the module we are importing from. That's the movie.py file. We are giving each movie the necessary attributes as
-# defined in our movie file (title, storyline, image URL, youtube URL, release date). At the end, we are using the fresh_tomatoes module,
-# which has a function entitled open_movies_page that takes the argument (movies). Movies, being our list of movies.
+# Here, we import the two other .py files/modules:
+# movie and fresh_tomatoes. Movie is where we have created
+# the appropriate classes. We can create a new instance of
+# a class by defining a variable as movie.Movie.
+# Movie with the upper-case is the class, and movie with
+# the lower case is the module we are importing from.
+# That's the movie.py file. We are giving each movie the necessary
+# attributes as defined in our movie file (title, storyline,
+# image URL, youtube URL, release date). At the end,
+# we are using the fresh_tomatoes module, which has a
+# function entitled open_movies_page that takes the
+# argument (movies). Movies, being our list of movies.
 
 lionheart = movie.Movie("Lionheart",
-                        "A Jean-Claude Van Damme film about never giving up, and fighting for what is right.",
+                        "A Jean-Claude Van Damme film about never giving up,"
+                        " and fighting for what is right.",
                         "https://upload.wikimedia.org/wikipedia/en/7/72/Lion-Heart-Poster.jpg",
                         "https://www.youtube.com/watch?v=QKqjwvWNnzg",
                         "1990")
@@ -20,13 +28,15 @@ uni_sol = movie.Movie("Universal Soldier",
                       "1992")
 
 cyborg = movie.Movie("Cyborg",
-                     "A post apocolyptic wasteland. Our only hope? Jean-Claude Van Damme.",
+                     "A post apocolyptic wasteland. Our only hope?"
+                     " Jean-Claude Van Damme.",
                      "https://upload.wikimedia.org/wikipedia/en/e/e4/Cyborgposter.jpg",
                      "https://www.youtube.com/watch?v=NsyjFihG5k8",
                      "1989")
 
 double_team = movie.Movie("Double Team",
-                          "Jean-Claude Van Damme and Dennis Rodman. What could go possibly go wrong?",
+                          "Jean-Claude Van Damme and Dennis Rodman."
+                          " What could go possibly go wrong?",
                           "https://upload.wikimedia.org/wikipedia/en/d/d6/Double_team_ver1.jpg",
                           "https://www.youtube.com/watch?v=36gSLmCHpUQ",
                           "1997")
@@ -38,7 +48,8 @@ death_warrant = movie.Movie("Death Warrant",
                             "1990")
 
 knock_off = movie.Movie("Knock Off",
-                        "Van Damme and Rob Schneider. This movie is not a knock off, but it's about fake foregin goods.",
+                        "Van Damme and Rob Schneider. This movie is not a"
+                        " knock off, but it's about fake foregin goods.",
                         "https://upload.wikimedia.org/wikipedia/en/1/14/Knockoffposter.jpg",
                         "https://www.youtube.com/watch?v=La6MF5EtuNc",
                         "1998")
--- movie.py
+++ movie.py
@@ -1,15 +1,21 @@
 import webbrowser
 
-# This creates a class called Movie. It is the main class where we store the information about each title. The __init__ function
-# includes the movie title, storyline, poster image, youtube trailer, and movie release date. We use these in fresh_tomatoes to generate
-# an HTML webpage with the info. The show_trailer function allows you to directly open the web browser and navigate to the youtube page
+# This creates a class called Movie. It is the main class
+# where we store the information about each title. The __init__ function
+# includes the movie title, storyline, poster image,
+# youtube trailer, and movie release date. We use these in
+# fresh_tomatoes to generate an HTML webpage with the info.
+# The show_trailer function allows you to directly open
+# the web browser and navigate to the youtube page
 # for the specific movie trailer.
 
+
 class Movie():
-    """This class is amazing. It lets us get all the information for our movie."""
+    "This class is amazing. It lets us get all the information for our movie."
     MOVIE_RATINGS = ["G", "PG", "PG-13", "R"]
 
-    def __init__(self, movie_title, movie_storyline, movie_poster, movie_trailer, release_date):
+    def __init__(self, movie_title, movie_storyline, movie_poster,
+                 movie_trailer, release_date):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = movie_poster
